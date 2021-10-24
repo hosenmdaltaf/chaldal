e5fc1abc640c02afb7d73f3ecb4c14e71abea1c7
hosenmdaltaf <altafhmeazee1999@outlook>
2021-10-24 11:55:03 +0600

    adding new features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Category from './Category';
 import Header from './Header';
+
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Category/>
     </div>
   );
 }
---
+++ src/Category.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Category() {
+    return (
+        <div>
+            <h1>Our Product Categories</h1>
+            
+        </div>
+    )
+}
+
+export default Category
